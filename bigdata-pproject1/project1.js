ae7dabf7e34e82b1dec386fc754735a7d3a68981
dani030 <47536968+dani030@users.noreply.github.com>
2019-10-21 21:39:45 +0300

    works

--- bigdata-pproject1/project1.js
+++ bigdata-pproject1/project1.js
@@ -83,65 +83,76 @@ var dbe = {
 
 
 
-var dbc2 = {
-    insert : function(document) {
+var insertClient = function(firstName1, secondName1, country1, city1, address1, telephone1, email1, code1) {
   
       
-      if(!document.firstName) {
-  
+      if(!firstName1) {  
         print('Property *firstName* is required');
        return; 
       }
   
-      if(!document.secondName) {
+      if(!secondName1) {
         print('Property *secondName* is required')
         return;
       }
-      if(!document.addressCollection.country) {
+      if(!country1) {
         print('Property *country * is required')
         return;
       }
-      if(!document.addressCollection.city) {
+      if(!city1) {
         print('Property *city* is required')
         return;
       }
-      if(!document.addressCollection.address) {
+      if(!address1) {
         print('Property *address* is required')
         return;
       }
-      if(!document.telephone) {
+      if(!telephone1) {
         print('Property *telephone* is required')
         return;
       }
-      if(!document.email) {
+      if(!email1) {
         print('Property *email* is required')
         return;
-      }
+      } 
+      if(!code1) {
+        print('Property *code* is required')
+        return;
+      }                 
       
-  
       // insert data 
-     db.Clients2.insert(document);
-
-     insertBankAccount2({code: document._id, clientId: document._id, currency: "BGN"})
-    
-    }
+     db.Clients2.insert({
+      firstName: firstName1, secondName: secondName1,  addressCollection : {   
+        country       : country1,
+        city          : city1,
+        address       : address1}, 
+        telephone: telephone1, 
+        email: email1
+     });
+     
+     db.bankAccounts2.insertOne({
+      code      : code1,
+      clientId : code1,      
+      currency  : 'BGN'
+    })
+     
   };
 
-  var insertBankAccount2 = function(code, clientId, currency) {
+  var insertBankAccount2 = function(code1, clientId1, currency1) {
     if(!code){
+       return;
+   }
+   if(!clientId){
         return;
     }
-    if(!clientId){
-        return;
-    }
-    var currencyVar
+    var currencyVar=currency1
     if(!currency){
         currencyVar='BGN'       
     }
-    db.bankAccounts2.insert({
-      code      : code,
-      clientId : clientId,      
-      currency  : currencyVar
+    db.bankAccounts2.insertOne({
+      code      : code1,
+      clientId : clientId1,      
+      currency  : currency1
     })
   };
   
@@ -149,10 +160,18 @@ var dbc2 = {
 
 
 
-  dbc2.insert({ firstName: 'Koljo Mamata', secondName: 'Koljo Mamata',  addressCollection : {   
+  dbc2.insert({ firstName: 'Jivko', secondName: 'Jivkov',  addressCollection : {   
     country       : 'Bulgaria',
     city          : 'Plovdiv',
     address       : 'Bjalo more'
-  }, telephone: '321321', email: 'fdsfds'
+  }, telephone: '99999999', email: 'aaaaaaaaa'
 
 });
+
+db.bankAccounts2.insert({
+  code      : 'dsadsg4543sa',
+  clientId : 'fdsfdsfds',      
+  currency  : 'frank'
+})
+
+insertClient('Mihail', 'Dimitrov', 'USA', 'Los Angeles', 'Obama', '0000022','dsadsa@adsa','IBANDsadsadsa')
